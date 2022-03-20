b5ab6d36edb6b807aa0eedfda1414a0867f26663
Categio <categio1@gmail.com>
2022-03-19 19:02:44 -0700

    Updated the Unformatting application to have full functionality. Works.

--- Telephone Unformat/Telephone Unformat/Form1.Designer.cs
+++ Telephone Unformat/Telephone Unformat/Form1.Designer.cs
@@ -32,25 +32,28 @@
             this.unformatButton = new System.Windows.Forms.Button();
             this.instructionLabel = new System.Windows.Forms.Label();
             this.numberTextBox = new System.Windows.Forms.TextBox();
+            this.clearButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // exitButton
             // 
-            this.exitButton.Location = new System.Drawing.Point(145, 106);
+            this.exitButton.Location = new System.Drawing.Point(190, 106);
             this.exitButton.Name = "exitButton";
             this.exitButton.Size = new System.Drawing.Size(75, 23);
             this.exitButton.TabIndex = 7;
-            this.exitButton.Text = "Exit";
+            this.exitButton.Text = "E&xit";
             this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
             // 
             // unformatButton
             // 
-            this.unformatButton.Location = new System.Drawing.Point(64, 106);
+            this.unformatButton.Location = new System.Drawing.Point(20, 106);
             this.unformatButton.Name = "unformatButton";
             this.unformatButton.Size = new System.Drawing.Size(75, 23);
             this.unformatButton.TabIndex = 6;
-            this.unformatButton.Text = "Unformat";
+            this.unformatButton.Text = "&Unformat";
             this.unformatButton.UseVisualStyleBackColor = true;
+            this.unformatButton.Click += new System.EventHandler(this.unformatButton_Click);
             // 
             // instructionLabel
             // 
@@ -68,11 +71,22 @@
             this.numberTextBox.Size = new System.Drawing.Size(100, 20);
             this.numberTextBox.TabIndex = 4;
             // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(104, 105);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(75, 23);
+            this.clearButton.TabIndex = 8;
+            this.clearButton.Text = "&Clear";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 140);
+            this.Controls.Add(this.clearButton);
             this.Controls.Add(this.exitButton);
             this.Controls.Add(this.unformatButton);
             this.Controls.Add(this.instructionLabel);
@@ -90,6 +104,7 @@
         private System.Windows.Forms.Button unformatButton;
         private System.Windows.Forms.Label instructionLabel;
         private System.Windows.Forms.TextBox numberTextBox;
+        private System.Windows.Forms.Button clearButton;
     }
 }
 
--- Telephone Unformat/Telephone Unformat/Form1.cs
+++ Telephone Unformat/Telephone Unformat/Form1.cs
@@ -15,5 +15,92 @@ namespace Telephone_Unformat
         {
             InitializeComponent();
         }
+
+        //IsValidFormat bool method: accepets str arg
+        //  determines if properly formatted as a US
+        //  telephone number. ie: (XXX)XXX-XXXX
+        //  If properly formatted, bool is true
+        //  else, false
+        private bool IsValidFormat(string str)
+        {
+            //length char count const variable
+            const int VALID_LENGTH = 13;
+
+            //bool to flag validity
+            bool valid;
+
+            //is str properly formatted?
+            if (str.Length == VALID_LENGTH && str[0] == '(' &&
+                     str[4] == ')' && str[8] == '-')
+            {
+                valid = true;
+            }
+            else
+            {
+                valid = false;
+            }
+            //return val of bool 
+            return valid;
+        }
+
+        //Unformat method: accepts str ref'd arg
+        //  assumed to have validly formatted phone #.
+        //  (see above example). Method removes the 
+        //  formatting symbols. ie. (,), & -. 
+        private void Unformat(ref string str)
+        {
+            //delete the paren at index 0
+            str = str.Remove(0, 1);
+
+            //delete the paren at index 3 
+            //  not at index 4 anymore since we 
+            //  deleted the first paren at index 0
+            str = str.Remove(3, 1);
+
+            //delete the - at index 6
+            //  not at index 8 anymore since we 
+            //  deleted the first paren at index 0
+            //  and the second paren at index 3
+            str = str.Remove(6, 1);
+        }
+
+        private void Clear()
+        {
+            MessageBox.Show("Clearing entered data?" +
+                "\nOK will proceed with this action.");
+            numberTextBox.Text = "";
+        }
+
+        private void unformatButton_Click(object sender, EventArgs e)
+        {
+            //trim and aquire user input
+            string input = numberTextBox.Text.Trim();
+
+            //if formatted propperly, unformat and display it.
+            if (IsValidFormat(input))
+            {
+                Unformat(ref input);
+
+                MessageBox.Show(input);
+            }
+            else
+            {
+                //show error message
+                MessageBox.Show("Invalid input." +
+                    "\nNot formatted properly, cannot proceed."+
+                    "\nPlease Try Again.");
+            }
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            Clear();
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            Clear();
+            this.Close();
+        }
     }
 }
